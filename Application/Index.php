4c32c482bf2919f4b2ad90d24580c6146f4cc2bd
Neoceryx <dani-deoz@hotmail.com>
2017-04-17 17:28:37 -0700

    Se agrego Materialize

---
+++ Application/Index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+  <html>
+
+    <head>
+      <?php include 'Web-Parts\NavBar.php'; ?>
+
+      <title>Warehouse</title>
+
+    </head>
+
+    <body>
+
+      <div class="row">
+
+        <div class="col s12 m4 l3 blue">
+          a
+        </div>
+
+        <div class="col s12 m8 l9 red">
+          Content Goes Here
+        </div>
+
+      </div>
+
+
+    </body>
+
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+  </html>
---
+++ Application/Web-Parts/NavBar.php
@@ -0,0 +1,26 @@
+<!--Import Google Icon Font-->
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+<!--Import materialize.css-->
+<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+<!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+<!-- Start NavBar -->
+<nav>
+  <div class="nav-wrapper">
+
+    <a href="#" class="brand-logo">Ware House</a>
+
+    <!-- Start Links -->
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+       <li><a href="#">Sass</a></li>
+       <li><a href="#">Components</a></li>
+       <li><a href="#">JavaScript</a></li>
+     </ul>
+    <!-- End Links -->
+
+  </div>
+</nav>
+<!-- End NavBar -->
