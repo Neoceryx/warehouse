a5fee7c0fff95c2ea8974345d13a90206b8f94cb
Neoceryx <dani-deoz@hotmail.com>
2017-04-19 19:43:46 -0700

    Se agrego el elemento en el dom que muestra la informacion obtenida de la base de datos

--- Application/Index.php
+++ Application/Index.php
@@ -12,12 +12,15 @@
 
       <div class="row">
 
-        <div class="col s12 m4 l3 blue">
+        <div class="col s12 m4 l3 ">
           a
         </div>
 
-        <div class="col s12 m8 l9 red">
-          Content Goes Here
+        <div class="col s12 m8 l9 ">
+
+          <!-- Display DataBase info From Ajax call -->
+          <div class="js_WareHouseResult row"></div>
+
         </div>
 
       </div>
@@ -28,5 +31,8 @@
     <!--Import jQuery before materialize.js-->
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
+
+    <!-- Aditional js files -->
+    <script type="text/javascript" src="js\Index\index.js"></script>
 
   </html>
