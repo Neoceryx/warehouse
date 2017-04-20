e78149f2c4e8bdf36b7c739bb53e6b1d0b7cb60c
Neoceryx <dani-deoz@hotmail.com>
2017-04-19 19:44:39 -0700

    Se separaron las consultas en regiones

--- DataBase/Queries.sql
+++ DataBase/Queries.sql
@@ -1,18 +1,38 @@
 Use Warehouse
 
+begin /* Region Add ArticlesTypes Informtion */
+
 insert into ArticlesTypes values ('Hardware');
 insert into ArticlesTypes values ('Software')
 
+end /* End Region */
+go;
+
+begin /* Region Add Articles test info */
 
 insert into Articles values ('Monitor Lenovo','123','m74',1);
 insert into Articles values ('Switch Cisco','456', 'C752',1);
 
-select * from Articles
+end /* End region */
+go;
 
-select Articles.Id,Articles.Description,SerialNumber,Model, ArticlesTypes.Description As 'Article type' from Articles
-INNER JOIN ArticlesTypes ON (ArticlesTypes_Id = ArticlesTypes.Id)
+begin /* Region add warehouse test info */ 
 
 insert into WareHouse values ('Jaula 1'), ('Jaula 2'), ('Jaula 3');
+
+end /* End Region */
+go;
+
+select * from Articles
+
+
+
+
+
+select Articles.Id, Articles.Description, SerialNumber,Model, ArticlesTypes.Description As 'Article type' from Articles
+INNER JOIN ArticlesTypes ON (ArticlesTypes_Id = ArticlesTypes.Id)
+
+
 
 select * from WareHouse
 
